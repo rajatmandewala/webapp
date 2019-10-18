1b146600fea868a63d2fd7ad06c120640d53911b
RR Mandewala <RR Mandewala@Lenovo-PC>
2019-10-18 14:35:24 +0530

    home controller added

---
+++ WEB-INF/classes/com/webapp/controller/HomeController.java
@@ -0,0 +1,11 @@
+
+package com.webapp.controller;
+
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+public class HomeController extends MultiActionController
+{
+    public String demo(){
+    return "df";
+    }
+}
